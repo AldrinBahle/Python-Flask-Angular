8345497d6639afd00946704a738f15676892bdb2
mxaba <samuel.mcebo@gmail.com>
2020-05-01 18:55:12 +0200

    Put your own creditaioons to test or run the APIs

---
+++ config.py
@@ -0,0 +1,11 @@
+from flask_mysqldb import MySQL
+from flask import Flask
+
+app = Flask(__name__)
+
+
+app.config['MYSQL_HOST'] = ''
+app.config['MYSQL_USER'] = ''
+app.config['MYSQL_PASSWORD'] = ''
+
+mysql = MySQL(app)
